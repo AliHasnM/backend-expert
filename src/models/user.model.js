655958d68482f0b4529bd29f69b4d1f6b72794b0
AliHasnM <mughal201burewala@gmail.com>
2024-06-23 10:56:31 +0500

    User and Video Model, Mongoose Aggregation Pipeline, Bcrypt and JWT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,9 @@
 // import mongoose
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"; // JWT for token generation
+import bcrypt from "bcrypt"; // bcrypt for password hashing
 
-// create mongoose Schema
+// Creating the user schema
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -19,7 +21,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -46,9 +48,70 @@ const userSchema = new mongoose.Schema(
       type: String,
     },
   },
-  { timestamps: true },
+  { timestamps: true }, // Automatically adds createdAt and updatedAt timestamps
 );
 
-// export mongoose model
+// Bcrypt: Password Hash
+// pre() me callback function k leye arrow function use krne se problem a sakti hain kue k arrow function me hamary pass this ka reference nhi hota context ka nhi pata hota esy tu pre() ko context ka pata hona bohat zarori hota hai kue k hum jis model par apply kr rahy hain os Object ki values ko access and modify krna hota hai (like userName:{type: String, required: true} wagara)
+// kis event par hook ko apply krna hai ye b batana hota hai (validate, save, remove, updateOne, deleteOne, init (note: init hooks are synchronous))
+// userSchema.pre("save", () => {}); // not prefer
+
+// Note: Pre-save hook to hash the password before saving the user document
+
+userSchema.pre("save", async function (next) {
+  // this reference k leye hai. isModified("string me hi value deni hoti hai") ye predefined function hai
+  if (!this.isModified("password")) return next();
+  // bcrypt k pass hash(passwordReference, NumberOfRound) function hota hai jis se password ko hash krta hai
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+}); // best method
+
+// Compare Password
+// methods hamy provide krta hai k hum customs method bana kr add kr sakty. isPasswordCorrect method banaya hum se jo password compare kry ga. bcrypt hi compare(data String, encrypted String) function se password ko compare krde ga
+
+// Note: Method to compare input password with hashed password in database
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// JWT
+// Generate Access Token: jwt sign(payload,ACCESS_TOKEN_SECRET,{ACCESS_TOKEN_EXPIRY}) function se token generate kr deta hai.
+
+// Note: Method to generate an access token using JWT
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+// Generate Refresh Token: payload se km information hoti hai es me
+
+// Note: Method to generate a refresh token using JWT
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
+// Exporting the User model
 export const User = mongoose.model("User", userSchema);
-// "User" model database me ja kr proluar aur lowercase ho jaie ga "users"
+
+// "User" model will be stored in the database as "users" (pluralized and lowercased)
---
+++ src/models/video.model.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema, plugin } from "mongoose";
+// plugin for pagination in aggregations
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+// Defining the schema for the video collection
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    view: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt timestamps
+  },
+);
+
+// Applying the pagination plugin to the schema
+videoSchema.plugin(mongooseAggregatePaginate);
+
+// Creating and exporting the Video model
+export const Video = mongoose.model("Video", videoSchema);
